c4d680bef92905bf3abeee1481a019ffaef0d49e
leandrocgsi <leandrocgsi@gmail.com>
2025-08-16 14:52:24 -0300

    REST Parameters and Exception Handler

--- 05_ParametersAndExceptionHandler/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Program.cs
+++ 05_ParametersAndExceptionHandler/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Program.cs
@@ -1,8 +1,12 @@
+using RestWithASPNET10Erudio.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<MathService>();
 
 var app = builder.Build();
 
