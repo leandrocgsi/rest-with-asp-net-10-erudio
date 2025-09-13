32adf406cd0a4a1bead6e04034745693befedea9
leandrocgsi <leandrocgsi@gmail.com>
2025-09-13 10:49:07 -0300

    Working with e-Mails in ASP.NET, MailKit and .NET 10 Apps

---
+++ 26_EMailsWithASP_NET/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Configurations/EmailConfig.cs
@@ -0,0 +1,32 @@
+﻿using RestWithASPNET10Erudio.Mail.Settings;
+
+namespace RestWithASPNET10Erudio.Configurations
+{
+    public static class EmailConfig
+    {
+        public static IServiceCollection AddEmailConfiguration(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var section = configuration.GetSection("Email");
+            var configs = section.Get<EmailSettings>();
+
+            if (configs == null)
+                throw new ArgumentNullException(
+                    nameof(configs),
+                    "Email configuration section is missing or invalid.");
+
+            configs.Username = Environment
+                .GetEnvironmentVariable("EMAIL_USERNAME")
+                ?? configs.Username;
+
+            configs.Password = Environment
+                .GetEnvironmentVariable("EMAIL_PASSWORD")
+                ?? configs.Password;
+
+            services.AddSingleton(configs);
+
+            return services;
+        }
+    }
+}
