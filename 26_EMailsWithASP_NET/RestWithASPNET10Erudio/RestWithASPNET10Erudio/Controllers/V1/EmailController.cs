0811faea9eb4e8c6d4d707be5cc80cb8a1fb492e
leandrocgsi <leandrocgsi@gmail.com>
2025-09-13 17:00:08 -0300

    Working with e-Mails in ASP.NET, MailKit and .NET 10 Apps

---
+++ 26_EMailsWithASP_NET/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Controllers/V1/EmailController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET10Erudio.Data.DTO.V1;
+using RestWithASPNET10Erudio.Services;
+
+namespace RestWithASPNET10Erudio.Controllers.V1
+{
+
+    [ApiController]
+    [Route("api/[controller]/v1")]
+    public class EmailController(
+        IEmailService emailService,
+        ILogger<EmailController> logger
+    ) : ControllerBase
+    {
+        private readonly IEmailService _emailService = emailService;
+        private readonly ILogger<EmailController> _logger = logger;
+
+        [HttpPost]
+        [ProducesResponseType(200, Type = typeof(string))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public IActionResult SendEmail(
+            [FromBody] EmailRequestDTO emailRequest
+        )
+        {
+            _logger.LogInformation("Sending email to {to}", emailRequest.To);
+            _emailService.SendSimpleEmail(
+                emailRequest.To,
+                emailRequest.Subject,
+                emailRequest.Body
+            );
+            return Ok("Email sent successfully");
+        }
+    }
+}
