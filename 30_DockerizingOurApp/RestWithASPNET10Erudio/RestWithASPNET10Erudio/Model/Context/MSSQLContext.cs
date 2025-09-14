38463d116a445ce1ba76557c210d30a424a2fd88
leandrocgsi <leandrocgsi@gmail.com>
2025-09-14 12:34:36 -0300

    Dockerizing our ASP.NET 10 RESTful API Application

---
+++ 30_DockerizingOurApp/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Model/Context/MSSQLContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestWithASPNET10Erudio.Model.Context
+{
+    public class MSSQLContext(DbContextOptions<MSSQLContext> options) : DbContext(options)
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
