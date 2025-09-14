29fbcacfac5e2317a3443d551ace5b700ad7c63c
leandrocgsi <leandrocgsi@gmail.com>
2025-09-14 10:59:13 -0300

    Shielding our API with FluentAssertions, Testcontainers, xUnit, Moq and ASP.NET 10

---
+++ 28_ShieldingOurAPIWithTests/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Services/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using RestWithASPNET10Erudio.Data.DTO.V1;
+
+namespace RestWithASPNET10Erudio.Services
+{
+    public interface IEmailService
+    {
+        void SendSimpleEmail(EmailRequestDTO emailRequest);
+        Task SendEmailWithAttachment(EmailRequestDTO emailRequest,
+            IFormFile attachment);
+    }
+}
