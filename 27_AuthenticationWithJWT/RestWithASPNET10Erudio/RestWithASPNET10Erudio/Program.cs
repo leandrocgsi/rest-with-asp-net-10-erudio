0213df345aaa844a33c89157cb081cf78aaf077d
leandrocgsi <leandrocgsi@gmail.com>
2025-09-14 10:57:19 -0300

    Implementing Authentication with ASP.NET 10 and JWT

--- 27_AuthenticationWithJWT/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Program.cs
+++ 27_AuthenticationWithJWT/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Program.cs
@@ -55,7 +55,11 @@ builder.Services.AddScoped<FileExporterFactory>();
 
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); 
 builder.Services.AddScoped<IFileServices, FileServicesImpl>();
+
 builder.Services.AddScoped<IPasswordHasher, Sha256PasswordHasher>();
+builder.Services.AddScoped<IUserAuthService, UserAuthServiceImpl>();
+builder.Services.AddScoped<ILoginService, LoginServiceImpl>();
+builder.Services.AddScoped<ITokenGenerator, TokenGenerator>();
 
 builder.Services.AddScoped<IPersonRepository, PersonRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
---
+++ 27_AuthenticationWithJWT/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Services/ILoginService.cs
@@ -0,0 +1,12 @@
+﻿using RestWithASPNET10Erudio.Data.DTO.V1;
+
+namespace RestWithASPNET10Erudio.Services
+{
+    public interface ILoginService
+    {
+        TokenDTO? ValidateCredentials(UserDTO user);
+        TokenDTO? ValidateCredentials(TokenDTO token);
+        bool RevokeToken(string username);
+        AccountCredentialsDTO Create(AccountCredentialsDTO user);
+    }
+}
