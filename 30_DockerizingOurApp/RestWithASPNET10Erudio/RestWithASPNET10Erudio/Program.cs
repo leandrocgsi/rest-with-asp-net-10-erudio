ff2660454b6268a70f40b92926d3de3298fc1d29
leandrocgsi <leandrocgsi@gmail.com>
2025-09-16 19:52:23 -0300

    Dockerizing our ASP.NET 10 RESTful API Application

--- 30_DockerizingOurApp/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Program.cs
+++ 30_DockerizingOurApp/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Program.cs
@@ -69,8 +69,10 @@ builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 app.UseRouting();
 
 app.UseAuthentication();
@@ -86,4 +88,6 @@ app.UseHATEOASRoutes();
 app.UseSwaggerSpecification();
 app.UseScalarConfiguration();
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Run($"http://*:{port}");
+// app.Run();
